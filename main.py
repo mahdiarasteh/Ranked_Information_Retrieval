ed58d509f4c2c783ddf6dafd37bdd2a2d9db86d2
mahdiarasteh <35062638+mahdiarasteh@users.noreply.github.com>
2022-06-13 18:35:58 +0430

    crawler

--- main.py
+++ main.py
@@ -1,31 +1,48 @@
+import requests
+from bs4 import BeautifulSoup
+from newspaper import Article
+from tqdm import tqdm
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-import numpy as np
-
-data = pd.read_csv('ana-101.csv')
 
 
-docs = data['title']
-# print(docs)
+def repeatitive(links, urls):
+    for link in links:
+        if link.a['href'] in urls :
+            return True
+    return False
 
-vectorizer = TfidfVectorizer()
-tfidf_docs = vectorizer.fit_transform(docs)
+
+def scrap_year(year : int):
+    page = 95
+    scrap_data = []
+    url_list = []
+    while True:
+        main_page_url = f"https://www.ana.press/page/archive.xhtml?mn=3&wide=0&dy=13&ms=0&pi={page}&yr={year}"
+        page += 1
+
+        html = requests.get(main_page_url).text
+        soup = BeautifulSoup(html, features='lxml')
+        links = soup.find_all('h3')
+
+        if repeatitive(links, url_list):
+            break
+
+        for link in tqdm(links):
+            page_url = "https://www.ana.press/" + link.a['href']
+            url_list.append(link.a['href'])
+            try:
+                article = Article(page_url)
+                article.download()
+                article.parse()
+                scrap_data.append({"url": page_url,
+                                   "title": article.title ,
+                                   "text": article.text})
+            except:
+                print(f"Failed to process page : {page_url}")
+
+    df = pd.DataFrame(scrap_data)
+    df.to_csv(f'ana-{page-1}.csv')
 
 
-# query
-query = 'دانشگاه تهران'
-tfidf_query = vectorizer.transform([query])[0]
-
-# similarities
-cosines = []
-for d in tfidf_docs:
-  cosines.append(
-    float(cosine_similarity(d, tfidf_query)))
-
-# sorting
-k = 10
-sorted_ids = np.argsort(cosines)
-for i in range(k):
-  cur_id = sorted_ids[-i-1]
-  print(docs[cur_id], cosines[cur_id])
+if __name__ == '__main__':
+    scrap_year(1401)
